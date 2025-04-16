817f8d07b0cc5abc60f5a921e9aee6e731fd80d0
Busayo <82906357+Busayo0@users.noreply.github.com>
2025-04-16 18:27:25 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,10 +1,9 @@
-import os
-import sys
 import subprocess
+import sys
 
 try:
     subprocess.Popen([sys.executable, "-m", "streamlit", "run", "app.py"])
 except Exception as e:
     import time
-    print("Error launching app:", e)
+    print("Failed to start Streamlit:", e)
     time.sleep(10)
